5ff28d316cee6d7639298befa6eb6e3d5602c0b5
João Vitor Cezario Sanches <joaovitormanolo@gmail.com>
2022-06-09 04:33:05 -0300

    feat : added db integration and sql tables creation file

---
+++ src/db/tables.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE "shortly"
+GRANT ALL PRIVILEGES ON DATABASE "shortly" TO postgres
+
+CREATE TABLE "users" (
+	"id" SERIAL PRIMARY KEY,
+	"name" TEXT NOT NULL,
+	"email" TEXT NOT NULL UNIQUE,
+	"password" TEXT NOT NULL,
+	"createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
+);
+
+CREATE TABLE "sessions" (
+	"id" SERIAL PRIMARY KEY,
+	"authToken" VARCHAR(36) NOT NULL UNIQUE,
+	"createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
+	"endedAt" TIMESTAMP,
+	"userId" INTEGER NOT NULL REFERENCES "users"("id"),
+);
+
+CREATE TABLE "urls" (
+	"id" SERIAL PRIMARY KEY,
+	"shortUrl" VARCHAR(21) NOT NULL UNIQUE,
+	"viewCount" BIGINT NOT NULL DEFAULT 0,
+	"createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
+	"deletedAt" TIMESTAMP,
+	"userId" INTEGER NOT NULL REFERENCES "users"("id"),
+);
